f3d9e80824692558d172dc46641f79939b73faba
Philipp <mockenhauptp@gmail.com>
2025-12-18 08:02:52 +0100

    Added two testfunctions for testing of ownership and borrowing functionality.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,38 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Update, testhello)
         .run();
+
 }
 
 fn testhello(){
     println!("Bevy is working !");
+    testrun();
+}
+
+fn testrun(){
+
+    //Ownership Testrun
+    let x =5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+    let z = String::from("hello");
+    println!("z = {}", z);
+    testrun2(z);
+    println!("z = {z}");
+
+
+    //Borrowing Testrun
+    let mut y = 6;
+
+    let p = &mut y;
+
+
+    *p = 7;
+    println!("p is {p}");
+
+}
+
+fn testrun2(string: String) {
+    println!("string is {}", string);
+
 }
